a1fdea4c16c41820dcdc2151ae9e0732e177acb9
Barret Schloerke <schloerke@gmail.com>
2018-04-20 12:32:46 -0400

    viztest against cran

--- scripts/viztest.R
+++ scripts/viztest.R
@@ -2,6 +2,4 @@ devtools::install_github("schloerke/viztest")
 # source("scripts/viztest.R")
 
 
-viztest::viztest(".", "bhaskarvk/leaflet.esri", resize = FALSE, stomp = TRUE)
-
-# viztest::viztest(".", "bhaskarvk/leaflet.esri", resize = FALSE, stomp = TRUE, skip_old = TRUE)
+viztest::viztest(".", "leaflet.esri", resize = FALSE, stomp = TRUE)
